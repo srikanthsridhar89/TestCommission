21780e1543c4cb0a44c8c3cb55fa99b05c5b6c13
srikanthd <srikanth.sridharr@gmail.com>
2020-05-18 15:04:56 +0530

    Second Commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub544/
+		
+		
+		String s="Commission-UI";
+		String s2=s.replaceAll(s, "-");
+		System.out.println(s2);
+
+	}
+
+}
