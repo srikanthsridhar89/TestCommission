3b3b467462371c5a8a2fd72bb346c57cfd1625be
vishu535 <vishveshwar.vishu@gmail.com>
2020-05-18 15:07:42 +0530

    added rectangle class pvr

---
+++ src/test/java/oopsexample/RectangleArea.java
@@ -0,0 +1,13 @@
+package oopsexample;
+
+public class RectangleArea {
+	public static void main(String args[]) {
+		int len = 8;
+		int bre = 5;
+		int area = len * bre;
+		System.out.println("Area of rectangle is "+area);
+		
+		
+	}
+
+}
